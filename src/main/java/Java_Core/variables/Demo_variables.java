2f5e694152f7e0d9f280ce7f9b7f29d333f1c49e
Mishitha97 <mishithav997@gmail.com>
2025-06-27 16:56:57 +0530

    variables

---
+++ src/main/java/Java_Core/variables/Demo_variables.java
@@ -0,0 +1,22 @@
+package Java_Core.variables;
+
+public class Demo_variables {
+    public static void main(String[] args) {
+//        int float double String char short long boolean
+        String name="Mishitha";
+        int Age=20;
+        float Height=5.4f;
+        String College="SONA COLLEGE";
+        char Gender='F';
+        String Place="Salem";
+//        System.out.println(name+"\n"+Age+"\n"+Height+"\n"+College+"\n"+Gender+"\n"+Place);
+        System.out.println("MY NAME IS :"+name);
+        System.out.println("Age is"+Age);
+        System.out.println("Height is"+Height);
+        System.out.println("College is:"+College);
+        System.out.println("Gender is"+Gender);
+        System.out.println("Place is"+Place);
+
+
+    }
+}
