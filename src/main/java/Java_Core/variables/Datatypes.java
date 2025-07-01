5fd2548377d4921fc4a321548428c6ca7452d086
Mishitha97 <mishithav997@gmail.com>
2025-07-01 16:59:13 +0530

    Datatypes and variables types

---
+++ src/main/java/Java_Core/variables/Datatypes.java
@@ -0,0 +1,23 @@
+package Java_Core.variables;
+
+
+public class Datatypes {
+    public static void main(String[] args) {
+        int value=10;
+        String name="Mishitha";
+        float height=5.4f;
+        long contact_no=96881893688l;
+        char Gender='F';
+
+        System.out.println(value);
+        System.out.println(name);
+        System.out.println(height);
+        System.out.println(contact_no);
+        System.out.println(Gender);
+//        comments
+        //single line and MultiLine comments
+        /*System.out.println("MISHITHA");
+        System.out.println("SONA");*/
+        System.out.println("Salem");  // to print the statements
+    }
+}
